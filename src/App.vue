809100e7c28d407b56eb7b59449baaa3a6e77409
M-Facey <faceymaxal17@gmail.com>
2023-02-20 22:44:03 -0500

    chore: added tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,9 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
+  <main class="h-screen bg-neutral-800 flex flex-col items-center justify-center">
+    <h2 class="text-6xl text-neutral-500 font-gloock">Octa Eureka</h2>
+    <p class="text-xl text-white pt-2 font-os">currently work in progress</p>
   </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
