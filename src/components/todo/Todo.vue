7a95507b0aac2e3bbdd52b0889fe9a8ce59b903d
M-Facey <faceymaxal17@gmail.com>
2023-02-21 01:34:10 -0500

    feat: added basic todo functionality

--- src/components/todo/Todo.vue
+++ src/components/todo/Todo.vue
@@ -1,14 +1,49 @@
 <script lang="ts" setup>
 import { ref } from "vue";
 
+import { nanoid } from "nanoid";
+
 import TodoInput from "./TodoInput.vue";
 import TodoButton from "./TodoButton.vue";
+import TodoItem from "./TodoItem.vue";
 
 import IconAdd from "../icons/IconAdd.vue";
 import IconFilter from "../icons/IconFilter.vue";
 import IconGrid from "../icons/IconGrid.vue";
 
+export interface Todo {
+  id: string;
+  name: string;
+  isCompleted: boolean;
+}
+
 const newTodo = ref("");
+
+const todos = ref<Todo[]>([]);
+
+const addTodo = () => {
+  todos.value.push({
+    id: nanoid(),
+    name: newTodo.value,
+    isCompleted: false,
+  });
+
+  newTodo.value = "";
+};
+
+const toggleIsCompleted = (id: string) => {
+  const completedStatus = todos.value.find(
+    (todo) => todo.id === id
+  )?.isCompleted;
+
+  todos.value[todos.value.findIndex((todo) => todo.id === id)].isCompleted =
+    !completedStatus;
+};
+
+const deleteTodo = (id: string) => {
+  console.log("asdasd");
+  todos.value = todos.value.filter((todo) => todo.id !== id);
+};
 </script>
 
 <template>
@@ -23,7 +58,12 @@ const newTodo = ref("");
         :is-optional="false"
         class="flex-grow"
       />
-      <todo-button button-label="Add Todo" button-size="sm" class="ml-auto">
+      <todo-button
+        button-label="Add Todo"
+        button-size="sm"
+        class="ml-auto"
+        @click="addTodo"
+      >
         <icon-add class="w-5" />
       </todo-button>
       <todo-button button-size="md" :is-icon-button="true">
@@ -32,6 +72,18 @@ const newTodo = ref("");
       <todo-button button-size="md" :is-icon-button="true">
         <icon-grid class="w-5" />
       </todo-button>
+    </div>
+
+    <div class="divide-y divide-neutral-800 flex flex-col">
+      <todo-item
+        v-for="todo in todos"
+        :todo-id="todo.id"
+        :todo-name="todo.name"
+        :is-completed="todo.isCompleted"
+        @toggle-completed="toggleIsCompleted(todo.id)"
+        @delete-task="deleteTodo(todo.id)"
+        class="py-3"
+      />
     </div>
   </div>
 </template>
---
+++ src/components/todo/TodoItem.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+import TodoButton from "./TodoButton.vue";
+
+import IconCheck from "../icons/IconCheck.vue";
+import IconClose from "../icons/IconClose.vue";
+
+export interface Prop {
+  todoId: string;
+  todoName: string;
+  isCompleted: boolean;
+}
+
+defineProps<Prop>();
+defineEmits<{
+  (e: "toggleCompleted"): void;
+  (e: "deleteTask", id: string): void;
+}>();
+</script>
+
+<template>
+  <div class="w-full flex items-center px-4">
+    <label
+      for="taskCheck"
+      class="relative w-6 h-6 flex items-center justify-center text-white border border-neutral-500 rounded"
+      @click="$emit('toggleCompleted')"
+    >
+      <input
+        :id="todoId"
+        type="checkbox"
+        class="absolute w-full h-full opacity-0 peer"
+        :checked="isCompleted"
+      />
+      <icon-check class="w-4 opacity-0 peer-checked:opacity-100" />
+    </label>
+
+    <p class="ml-2 mr-auto text-neutral-300">{{ todoName }}</p>
+
+    <todo-button button-size="md" @click="$emit('deleteTask', todoId)">
+      <icon-close class="w-5" />
+    </todo-button>
+  </div>
+</template>
