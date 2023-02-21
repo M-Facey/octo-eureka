7c3392e62e06b5213718df2be9b296722a156d5f
M-Facey <faceymaxal17@gmail.com>
2023-02-21 00:13:36 -0500

    feat: added header component

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup></script>
+
+<template>
+  <header class="absolute h-[35vh] w-full  flex flex-col items-center justify-end bg-neutral-800 pb-28">
+    <h1 class="text-6xl text-neutral-500 font-gloock">Octa Eureka</h1>
+    <p class="text-xl text-neutral-200 font-os mt-3">A simple <span class="bg-neutral-300 text-neutral-800 px-2 py-0.5 rounded-lg">and efficient way</span>  to do a <span class="font-extrabold text-red-500">REAL</span>  todo.</p>
+  </header>
+</template>
