0d1745b9c5cd27e56d36bb7aae7fed2eeeb15877
M-Facey <faceymaxal17@gmail.com>
2023-02-22 11:13:39 -0500

    fix: changed assets folder to `dist`

--- release.config.js
+++ release.config.js
@@ -6,7 +6,7 @@ const config = {
     [
       "@semantic-release/git",
       {
-        assets: ["dist/*.js", "dist/"],
+        assets: ["dist"],
         messages:
           "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
