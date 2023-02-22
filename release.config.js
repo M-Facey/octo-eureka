c104b50f0c813de74c55cdfcea681efcf0d9fe74
M-Facey <faceymaxal17@gmail.com>
2023-02-22 11:21:33 -0500

    fix: typo in release config

--- release.config.js
+++ release.config.js
@@ -1,7 +1,7 @@
 const config = {
   branches: ["main"],
   plugins: [
-    "@semantic-release/commit-analyser",
+    "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     [
       "@semantic-release/git",
