1ae94acc4bdfed81a89b8238fbadb5d751cf0352
M-Facey <faceymaxal17@gmail.com>
2023-02-21 21:17:40 -0500

    feat: added todo item unit test

---
+++ src/__tests__/components/TodoItem.test.ts
@@ -0,0 +1,16 @@
+import { mount } from "@vue/test-utils";
+import { describe, it, expect } from "vitest";
+import TodoItem from "../../components/todo/TodoItem.vue";
+
+describe("Todo Item Component", () => {
+  it("shoul render", () => {
+    const wrapper = mount(TodoItem, {
+      props: {
+        todoId: "1",
+        todoName: "Simple Task",
+        isCompleted: true,
+      },
+    });
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+});
