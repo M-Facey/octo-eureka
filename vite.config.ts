28607945239c5479a150e62b25daed7a1cd2ccdd
M-Facey <faceymaxal17@gmail.com>
2023-02-21 21:05:15 -0500

    chore: added vitest & @vue/test-utils

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,22 @@
-import { fileURLToPath, URL } from 'node:url'
+/// <reference types="vitest" />
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  test: {
+    environment: "jsdom",
+    include: ["**/__tests__/**/*.test.ts"],
+    coverage: {
+      provider: "c8",
+    },
+  },
+});
