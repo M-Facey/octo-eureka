11697574759371141334e4c4116183a163cfa388
M-Facey <faceymaxal17@gmail.com>
2023-02-21 21:05:42 -0500

    feat: added todo button unit test

---
+++ src/__tests__/components/TodoButton.test.ts
@@ -0,0 +1,15 @@
+import { mount } from "@vue/test-utils";
+import { describe, it, expect } from "vitest";
+import TodoButton from "@/components/todo/TodoButton.vue";
+
+describe("Todo Button Component", () => {
+  it("should render", () => {
+    const wrapper = mount(TodoButton, {
+      props: {
+        buttonLabel: "Button",
+        buttonSize: "lg",
+      },
+    });
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+});
