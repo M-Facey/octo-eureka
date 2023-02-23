0ca65cd14297530812c12f4342b4600ab44388e2
M-Facey <faceymaxal17@gmail.com>
2023-02-23 00:00:51 -0500

    fix: button background and updated size

--- src/components/todo/TodoButton.vue
+++ src/components/todo/TodoButton.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 export interface Prop {
   buttonLabel?: string;
-  buttonSize: "lg" | "md" | "sm";
+  buttonSize: "lg" | "md" | "sm" | "xs";
   isIconButton?: boolean;
   isDisabled?: boolean;
 }
@@ -19,13 +19,13 @@ defineEmits<{ (e: "triggerEvent"): void }>();
   <button
     class="text-neutral-300 rounded flex gap-1"
     :class="{
-      'px-2 py-2': buttonSize === 'sm',
+      'px-1.5 py-0.5': buttonSize === 'xs',
+      'p-2': buttonSize === 'sm',
       'px-3 py-2': buttonSize === 'md',
       'px-8 py-4': buttonSize === 'lg',
-      'bg-transparent hover:bg-neutral-800': isIconButton,
-      'bg-neutral-800 hover:bg-neutral-700': !isIconButton,
-      'font-medium text-neutral-600 bg-neutral-800': isDisabled,
-      'text-neutral-300 bg-neutral-800': !isDisabled,
+      'font-medium text-neutral-600 bg-neutral-700': isDisabled,
+      'bg-neutral-700 hover:bg-neutral-900': isIconButton,
+      'bg-neutral-800 hover:bg-neutral-700': !isIconButton && !isDisabled,
     }"
     :disabled="isDisabled"
     @click="$emit('triggerEvent')"
