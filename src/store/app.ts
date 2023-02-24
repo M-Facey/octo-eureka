1e855f00fb9e6f34c4e07d30a3e8f34fd9fdaac7
M-Facey <faceymaxal17@gmail.com>
2023-02-24 00:19:26 -0500

    feat: added sort functionality

--- src/store/app.ts
+++ src/store/app.ts
@@ -6,17 +6,42 @@ export const useAppStore = defineStore({
   state: () => ({
     todos: [] as Todo[],
     viewingStatus: "all",
-    showFilterModal: false,
+    showModal: "",
+    sortBy: "oldest",
   }),
   getters: {
     getTodosByStatus(): Todo[] {
       if (this.viewingStatus === "completed") {
-        return this.todos.filter((todo) => todo.isCompleted);
+        return this.getTodoBySortOrder.filter((todo) => todo.isCompleted);
       } else if (this.viewingStatus === "ongoing") {
-        return this.todos.filter((todo) => !todo.isCompleted);
+        return this.getTodoBySortOrder.filter((todo) => !todo.isCompleted);
       }
 
-      return this.todos;
+      return this.getTodoBySortOrder;
+    },
+    getTodoBySortOrder(): Todo[] {
+      if (this.sortBy === "nameAsc") {
+        return this.getTodosByStatus.sort((a, b) =>
+          a.name.localeCompare(b.name)
+        );
+      }
+
+      if (this.sortBy === "nameDesc") {
+        return this.getTodosByStatus.sort((a, b) =>
+          b.name.localeCompare(a.name)
+        );
+      }
+
+      if (this.sortBy === "newest") {
+        return this.todos.sort(
+          (a, b) => b.createdAt.getTime() - a.createdAt.getTime()
+        );
+      }
+
+      // returns the oldest by default
+      return this.todos.sort(
+        (a, b) => a.createdAt.getTime() - b.createdAt.getTime()
+      );
     },
     getTotalOnGoingTodos(): number {
       return this.todos.filter((todo) => !todo.isCompleted).length;
@@ -45,8 +70,16 @@ export const useAppStore = defineStore({
     changeStatus(status: string) {
       this.viewingStatus = status;
     },
-    toggleShowFilterModal() {
-      this.showFilterModal = !this.showFilterModal;
+    setShowModal(modalName: string) {
+      if (this.showModal === modalName) {
+        this.showModal = "";
+        return;
+      }
+      this.showModal = modalName;
+    },
+    setSortBy(sortOrder: string) {
+      if (this.sortBy === sortOrder) return;
+      this.sortBy = sortOrder;
     },
   },
 });
