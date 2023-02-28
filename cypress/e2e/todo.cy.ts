0cc2c295be919ad0c27428c6cee99bb16f107754
M-Facey <faceymaxal17@gmail.com>
2023-02-27 21:53:43 -0500

    feat: added cypress for e2e tests

---
+++ cypress/e2e/todo.cy.ts
@@ -0,0 +1,17 @@
+describe("Todo Tests", () => {
+  it("should able to add a single todo", () => {
+    // test script to be added later
+  });
+
+  it("should able to add multiple todos", () => {
+    // test script to be added later
+  });
+
+  it("should able to complete todo", () => {
+    // test script to be added later
+  });
+
+  it("should able to multiple complete todos", () => {
+    // test script to be added later
+  });
+});
