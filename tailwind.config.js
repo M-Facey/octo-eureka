38cb5abd00b7dc2b8e783756db9ba82b8616a15e
M-Facey <faceymaxal17@gmail.com>
2023-02-28 09:45:27 -0500

    fix: added darkMode config to tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        'gloock': ["Gloock", "serif"],
-        'os': ["Open Sans", "san-serif"],
-      }
+        gloock: ["Gloock", "serif"],
+        os: ["Open Sans", "san-serif"],
+      },
     },
   },
   plugins: [],
