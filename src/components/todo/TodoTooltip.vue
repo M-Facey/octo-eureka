027fcc08ebfe5cb668972e89694309dabb425921
M-Facey <faceymaxal17@gmail.com>
2023-02-28 23:13:28 -0500

    fix: updated todo tooltip styles

--- src/components/todo/TodoTooltip.vue
+++ src/components/todo/TodoTooltip.vue
@@ -7,7 +7,7 @@ defineProps<Prop>();
 
 <template>
   <p
-    class="tooltip absolute -top-3 -translate-y-full left-1/2 -translate-x-1/2 text-neutral-900 dark:text-neutral-100 bg-neutral-200 dark:bg-neutral-700 px-2 py-1 rounded"
+    class="tooltip w-max absolute -top-3 -translate-y-full left-1/2 -translate-x-1/2 text-neutral-900 dark:text-neutral-100 bg-neutral-200 dark:bg-neutral-700 px-2 py-1 rounded"
   >
     {{ text }}
   </p>
@@ -16,7 +16,6 @@ defineProps<Prop>();
 <style scoped>
 .tooltip::before {
   content: "";
-  /* border: 6px solid transparent; */
   @apply absolute -bottom-[12px] left-1/2 -translate-x-1/2;
   @apply border-[6px] border-transparent border-t-neutral-200 dark:border-t-neutral-700;
 }
