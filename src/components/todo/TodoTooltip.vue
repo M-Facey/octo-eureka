335d451c56dcc9967bea482c3b9620822f24703d
M-Facey <faceymaxal17@gmail.com>
2023-02-28 19:04:25 -0500

    fix: theme based styling on todo tooltip

--- src/components/todo/TodoTooltip.vue
+++ src/components/todo/TodoTooltip.vue
@@ -7,7 +7,7 @@ defineProps<Prop>();
 
 <template>
   <p
-    class="tooltip absolute -top-3 -translate-y-full left-1/2 -translate-x-1/2 bg-neutral-500 dark:bg-neutral-700 px-2 py-1 rounded"
+    class="tooltip absolute -top-3 -translate-y-full left-1/2 -translate-x-1/2 text-neutral-900 dark:text-neutral-100 bg-neutral-200 dark:bg-neutral-700 px-2 py-1 rounded"
   >
     {{ text }}
   </p>
@@ -18,6 +18,6 @@ defineProps<Prop>();
   content: "";
   /* border: 6px solid transparent; */
   @apply absolute -bottom-[12px] left-1/2 -translate-x-1/2;
-  @apply border-[6px] border-transparent border-t-neutral-700;
+  @apply border-[6px] border-transparent border-t-neutral-200 dark:border-t-neutral-700;
 }
 </style>
