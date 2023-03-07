ab16a56e680d8fa67e6a183f1e6ed4c9f2e9cba7
M-Facey <faceymaxal17@gmail.com>
2023-03-06 21:19:20 -0500

    fix: updated breaking point in composable

--- src/composables/useScreenSize.ts
+++ src/composables/useScreenSize.ts
@@ -2,14 +2,22 @@ import { useBreakpoints } from "@vueuse/core";
 
 export default () => {
   const breakpoints = useBreakpoints({
+    mobileSm: "375px",
+    mobileMd: "450px",
     mobile: "640px",
   });
 
-  const onMobile = breakpoints.smaller("mobile");
+  const onMobileSm = breakpoints.smallerOrEqual("mobileSm");
+  const onMobileMd = breakpoints.smallerOrEqual("mobileMd");
+  const onMobile = breakpoints.smallerOrEqual("mobile");
   const onDesktop = breakpoints.greaterOrEqual("mobile");
+  const betweenMobileSmAndMd = breakpoints.between("mobileSm", "mobileMd");
 
   return {
-    onMobile: onMobile,
-    onDesktop: onDesktop,
+    onMobileSm,
+    onMobileMd,
+    onMobile,
+    onDesktop,
+    betweenMobileSmAndMd,
   };
 };
