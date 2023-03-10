8bdb5d0c97aaf5105ff2dafe34c96d42028ff655
M-Facey <faceymaxal17@gmail.com>
2023-03-10 02:21:03 -0500

    fix: updated styling in app component

--- src/App.vue
+++ src/App.vue
@@ -34,7 +34,7 @@ watch(
 </script>
 
 <template>
-  <main class="h-screen bg-neutral-400 overflow-hidden">
+  <main class="relative h-screen bg-neutral-400 overflow-hidden">
     <the-header />
     <todo />
   </main>
