28a8741007d9cef83ef2bb2c5ceba13264cfb61e
M-Facey <faceymaxal17@gmail.com>
2023-03-12 22:10:28 -0400

    feat: added cypress tests

--- cypress/e2e/todo.cy.ts
+++ cypress/e2e/todo.cy.ts
@@ -1,17 +1,111 @@
 describe("Todo Tests", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("should not add todo if input is empty", () => {
+    cy.get("input[data-cy=todoInput]").should("have.text", "");
+    cy.get("button[data-cy=addTodo]").should("be.disabled");
+  });
+
   it("should able to add a single todo", () => {
-    // test script to be added later
+    cy.get("input[data-cy=todoInput]").clear().type("Clean the floor");
+    cy.get("button[data-cy=addTodo]").click();
+
+    cy.get("div[data-cy=todo]").should("exist");
+    cy.get("div[data-cy=todo]")
+      .find("p")
+      .should("have.text", "Clean the floor");
   });
 
   it("should able to add multiple todos", () => {
-    // test script to be added later
+    cy.get("input[data-cy=todoInput]").clear().type("Clean the floor");
+    cy.get("button[data-cy=addTodo]").click();
+    cy.get("input[data-cy=todoInput]")
+      .clear()
+      .type("Wash the dishes")
+      .type("{enter}");
+
+    cy.get("div[data-cy=todo]").should("exist").should("have.length", 2);
+    cy.get("div[data-cy=todo]")
+      .first()
+      .find("p")
+      .should("have.text", "Clean the floor");
+    cy.get("div[data-cy=todo]")
+      .last()
+      .find("p")
+      .should("have.text", "Wash the dishes");
   });
 
   it("should able to complete todo", () => {
-    // test script to be added later
+    cy.get("input[data-cy=todoInput]").clear().type("Clean the floor");
+    cy.get("button[data-cy=addTodo]").click();
+    cy.get("div[data-cy=todo]").find("label[data-cy=todoCheck]").click();
+
+    cy.get("div[data-cy=todo]")
+      .find("label[data-cy=todoCheck]")
+      .find("svg")
+      .should("exist");
+    cy.get("div[data-cy=todo]").find("p").should("have.class", "completed");
+
+    cy.get("span[data-cy=totalOngoingTodos]").should("have.text", "0");
+    cy.get("span[data-cy=totalTodos").should("have.text", "1");
   });
 
   it("should able to multiple complete todos", () => {
-    // test script to be added later
+    cy.get("input[data-cy=todoInput]").clear().type("Clean the floor");
+    cy.get("button[data-cy=addTodo]").click();
+    cy.get("input[data-cy=todoInput]")
+      .clear()
+      .type("Wash the dishes")
+      .type("{enter}");
+
+    cy.get("div[data-cy=todo]")
+      .first()
+      .find("label[data-cy=todoCheck]")
+      .click();
+    cy.get("div[data-cy=todo]")
+      .first()
+      .find("label[data-cy=todoCheck]")
+      .find("svg")
+      .should("exist");
+    cy.get("div[data-cy=todo]")
+      .first()
+      .find("p")
+      .should("have.class", "completed");
+
+    cy.get("div[data-cy=todo]").last().find("label[data-cy=todoCheck]").click();
+    cy.get("div[data-cy=todo]")
+      .last()
+      .find("label[data-cy=todoCheck]")
+      .find("svg")
+      .should("exist");
+    cy.get("div[data-cy=todo]")
+      .last()
+      .find("p")
+      .should("have.class", "completed");
+
+    cy.get("span[data-cy=totalOngoingTodos]").should("have.text", "0");
+    cy.get("span[data-cy=totalTodos").should("have.text", "2");
+  });
+
+  it("should be able to delete a todo", () => {
+    cy.get("input[data-cy=todoInput]").clear().type("Clean the floor");
+    cy.get("button[data-cy=addTodo]").click();
+
+    cy.get("div[data-cy=todo]").find("button[data-cy=deleteTodo]").click();
+
+    cy.get("div[data-cy=todo]").should("not.exist");
+    cy.get("span[data-cy=totalTodos").should("have.text", "0");
+  });
+
+  it("should be able to delete a todo", () => {
+    cy.get("input[data-cy=todoInput]").clear().type("Clean the floor");
+    cy.get("button[data-cy=addTodo]").click();
+
+    cy.get("div[data-cy=todo]").find("button[data-cy=deleteTodo]").click();
+
+    cy.get("div[data-cy=todo]").should("not.exist");
+    cy.get("span[data-cy=totalTodos").should("have.text", "0");
   });
 });
