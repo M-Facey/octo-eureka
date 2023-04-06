0ca15adc3b690b0fa7b4e2e68d5b78c3e4843fca
M-Facey <faceymaxal17@gmail.com>
2023-04-06 11:15:58 -0500

    chore: added custom scrollbar

--- src/components/modal/TagModal.vue
+++ src/components/modal/TagModal.vue
@@ -16,41 +16,62 @@ const changeStatus = (option: ModalOption) => {
 
 <template>
   <div
-    class="absolute top-full -right-3 translate-y-3 w-max h-[100px] flex flex-col gap-1 bg-neutral-300 dark:bg-neutral-800 border border-neutral-700 dark:border-neutral-700/70 p-2 cursor-default overflow-x-auto rounded-lg"
+    class="absolute top-full -right-3 translate-y-3 w-max max-h-[100px] flex flex-col gap-1 bg-neutral-300 dark:bg-neutral-800 border border-neutral-700 dark:border-neutral-700/70 p-2 cursor-default overflow-y-auto overflow-x-hidden rounded-lg"
   >
-    <div
-      v-for="option in modalStore.modalOptions"
-      class="flex items-center gap-x-2"
-    >
-      <label
-        :for="option.value"
-        class="relative w-4 h-4 flex items-center justify-center text-neutral-900 dark:text-white border border-neutral-500 hover:bg-neutral-400/30 dark:hover:bg-neutral-900 cursor-pointer rounded group/filter"
-        :data-cy="option.value"
-        @click.stop="changeStatus(option)"
+    <div class="custom_scroll h-full pr-2 overflow-y-auto">
+      <div
+        v-for="option in modalStore.modalOptions"
+        class="flex items-center gap-x-2"
       >
-        <input
-          :id="option.value"
-          type="radio"
-          class="absolute w-full h-full opacity-0 peer"
-          :checked="option.value === modalStore.currentModalOption"
-        />
-        <icon-check
-          class="w-3 opacity-0 peer-checked:opacity-100 group-hover/filter:opacity-40"
-        />
-      </label>
-      <p
-        class="cursor-pointer"
-        :class="{
-          'text-neutral-500 hover:text-neutral-400':
-            option.value !== modalStore.currentModalOption,
-          'font-bold text-neutral-900 dark:text-neutral-300 ':
-            option.value === modalStore.currentModalOption,
-        }"
-        :data-cy="`${option.value}Label`"
-        @click="changeStatus(option)"
-      >
-        {{ option.displayName }}
-      </p>
+        <label
+          :for="option.value"
+          class="relative w-4 h-4 flex items-center justify-center text-neutral-900 dark:text-white border border-neutral-500 hover:bg-neutral-400/30 dark:hover:bg-neutral-900 cursor-pointer rounded group/filter"
+          :data-cy="option.value"
+          @click.stop="changeStatus(option)"
+        >
+          <input
+            :id="option.value"
+            type="radio"
+            class="absolute w-full h-full opacity-0 peer"
+            :checked="option.value === modalStore.currentModalOption"
+          />
+          <icon-check
+            class="w-3 opacity-0 peer-checked:opacity-100 group-hover/filter:opacity-40"
+          />
+        </label>
+        <p
+          class="cursor-pointer"
+          :class="{
+            'text-neutral-500 hover:text-neutral-400':
+              option.value !== modalStore.currentModalOption,
+            'font-bold text-neutral-900 dark:text-neutral-300 ':
+              option.value === modalStore.currentModalOption,
+          }"
+          :data-cy="`${option.value}Label`"
+          @click="changeStatus(option)"
+        >
+          {{ option.displayName }}
+        </p>
+      </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.custom_scroll::-webkit-scrollbar-track {
+  -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+  border-radius: 10px;
+  @apply bg-black/5 dark:bg-white/10;
+  /* background-color: #f5f5f552; */
+}
+
+.custom_scroll::-webkit-scrollbar {
+  width: 5px;
+}
+
+.custom_scroll::-webkit-scrollbar-thumb {
+  border-radius: 10px;
+  -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+  @apply bg-blue-500;
+}
+</style>
