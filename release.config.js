5d1f653b3b5df33dfc3db0b7215f6156b871ce5d
M-Facey <faceymaxal17@gmail.com>
2023-04-09 17:31:35 -0500

    chore: added `@semantic-release/npm`

--- release.config.js
+++ release.config.js
@@ -3,6 +3,7 @@ const config = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
     [
       "@semantic-release/git",
       {
